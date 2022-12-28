605184f358bf38c9a3b6ed977c573c23808ad935
phychee <mvzhussupova@gmail.com>
2022-12-28 15:12:43 +0300

    Adding pop-up window

--- app/src/main/java/com/example/helloworld/PopActivity.kt
+++ app/src/main/java/com/example/helloworld/PopActivity.kt
@@ -1,11 +1,32 @@
 package com.example.helloworld
 
+import android.app.Activity
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.WindowInsets
 
-class PopActivity : AppCompatActivity() {
+class PopActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pop)
+
+        var width: Int = 0
+        var height: Int = 0
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            val windowMetrics = this.windowManager.currentWindowMetrics
+            val insets = windowMetrics.windowInsets
+                .getInsetsIgnoringVisibility(WindowInsets.Type.systemBars())
+            height = windowMetrics.bounds.width() - insets.bottom - insets.top
+            width = windowMetrics.bounds.height() - insets.left - insets.right
+        } else {
+            val dm = DisplayMetrics()
+            windowManager.defaultDisplay.getMetrics(dm)
+            height = dm.heightPixels
+            width = dm.widthPixels
+        }
+
+        window.setLayout((width*.35).toInt(), (height*.7).toInt())
     }
 }
